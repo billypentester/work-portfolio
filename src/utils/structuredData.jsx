9029ce75fb09aa0f5e19d0a57acd4c246b1aa6ee
Bilal Ahmad <bilalsheikh2500@gmail.com>
2024-01-30 22:54:29 +0500

    validation

--- src/utils/structuredData.jsx
+++ src/utils/structuredData.jsx
@@ -1,30 +1,37 @@
 import React from 'react';
 
 const ArticleStructuredData = () => {
-    const structuredData = {
-      '@context': 'https://schema.org',
-      '@type': 'Person',
-      name: 'Bilal Ahmad',
-      url: 'https://work.billypentester.vercel.app/',
-      image: 'https://work.billypentester.vercel.app/assets/lightPic-z2uj6qnq.webp',
-      sameAs: [
-        'https://work.billypentester.vercel.app',
-        'https://github.com/billypentester',
-        'https://www.linkedin.com/in/billypentester'
-      ],
-      jobTitle: 'Software Engineer',
-      worksFor: {
-        '@type': 'Organization',
-        name: 'Simplex Technology Solutions'
-      },
-      'address': {
-        '@type': 'PostalAddress',
-        addressLocality: 'Lahore',
-        addressRegion: 'Punjab'
-      }
-    };
+
+  const structuredData = {
+    '@context': 'https://schema.org',
+    '@type': 'Person',
+    name: 'Bilal Ahmad',
+    jobTitle: 'Software Engineer',
+    url: 'https://work.billypentester.vercel.app/',
+    image: 'https://work.billypentester.vercel.app/assets/lightPic-z2uj6qnq.webp',
+    sameAs: [
+      'https://www.linkedin.com/in/billypentester',
+      'https://github.com/billypentester'
+    ],
+    alumniOf: {
+      '@type': 'EducationalOrganization',
+      name: 'COMSATS University Islamabad',
+      sameAs: 'https://lahore.comsats.edu.pk',
+    },
+    worksFor: {
+      '@type': 'Organization',
+      name: 'Simplex Technology Solutions',
+      url: 'https://simplexts.net/',
+    },
+    address: {
+      '@type': 'PostalAddress',
+      addressLocality: 'Lahore',
+      addressCountry: 'Pakistan',
+    },
+  };
       
-    return <script type="application/ld+json">{JSON.stringify(structuredData)}</script>;
+  return <script type="application/ld+json">{JSON.stringify(structuredData)}</script>;
+
 };
 
 export default ArticleStructuredData;
