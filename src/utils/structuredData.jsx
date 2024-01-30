5f644ed7b3ab03987ce9fac2664dfa48b55172b6
Bilal Ahmad <bilalsheikh2500@gmail.com>
2024-01-30 22:59:37 +0500

    modified

--- src/utils/structuredData.jsx
+++ src/utils/structuredData.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 
 const ArticleStructuredData = () => {
-
   const structuredData = {
     '@context': 'https://schema.org',
     '@type': 'Person',
     name: 'Bilal Ahmad',
     jobTitle: 'Software Engineer',
     url: 'https://work.billypentester.vercel.app/',
-    image: 'https://work.billypentester.vercel.app/assets/lightPic-z2uj6qnq.webp',
     sameAs: [
       'https://www.linkedin.com/in/billypentester',
       'https://github.com/billypentester'
+    ],
+    knowsAbout: [
+        'React js',
+        'Next js',
+        'Vue js',
+        'Node js',
+        'Nest js',
+        'Express js',
+        'MongoDB',
+        'MySQL',
+        'Postgresql',
+        'Redis'
     ],
     alumniOf: {
       '@type': 'EducationalOrganization',
@@ -23,15 +33,27 @@ const ArticleStructuredData = () => {
       name: 'Simplex Technology Solutions',
       url: 'https://simplexts.net/',
     },
+    hasOccupation: 'Full Stack Developer',
+    offers: {
+      '@type': 'OfferCatalog',
+      name: 'Services Offered',
+      itemListElement: [
+        {
+          '@type': 'Offer',
+          itemOffered: 'Web App Development',
+          description: 'I will be creating beautiful, responsive and fast web apps for you.',
+          price: '1000',
+          availability: 'https://schema.org/InStock',
+        }
+      ],
+    },
     address: {
       '@type': 'PostalAddress',
       addressLocality: 'Lahore',
       addressCountry: 'Pakistan',
     },
   };
-      
   return <script type="application/ld+json">{JSON.stringify(structuredData)}</script>;
-
 };
 
 export default ArticleStructuredData;
